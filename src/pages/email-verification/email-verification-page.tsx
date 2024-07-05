d59c0dbfd9b7d30453b3ae3840a4e36cc997ac4a
yashim-deriv <yashim@regentmarkets.com>
2024-07-05 16:29:07 +0800

    feat: add tailwindcss

--- src/pages/email-verification/email-verification-page.tsx
+++ src/pages/email-verification/email-verification-page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export const EmailVerificationPage = () => {
-    return <React.Fragment>Email Verification Page</React.Fragment>;
+    return (
+        <React.Fragment>
+            <div className="font-semibold">Email Verification Page</div>
+        </React.Fragment>
+    );
 };
