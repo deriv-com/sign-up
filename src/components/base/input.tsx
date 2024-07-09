3e6f101e06e884fbf8910c4f15c4ee6f71149f7c
yashim-deriv <yashim@regentmarkets.com>
2024-07-09 13:57:32 +0800

    feat: completed email verification screen

--- src/components/base/input.tsx
+++ src/components/base/input.tsx
@@ -1,8 +1,29 @@
 import clsx from "clsx";
 import { ComponentProps } from "react";
 
-interface InputProps extends ComponentProps<"input"> {}
+interface InputProps extends Omit<ComponentProps<"input">, "size"> {
+    block?: boolean;
+    size?: "sm" | "md" | "lg";
+}
 
-export const Input = ({ className, ...props }: InputProps) => {
-    return <input className={clsx("", className)} {...props} />;
+export const Input = ({ className, block = false, size = "md", ...props }: InputProps) => {
+    return (
+        <input
+            className={clsx(
+                className,
+                "border border-gray-300 rounded",
+                { "w-full": block },
+                {
+                    "h-8": size === "sm",
+                    "h-10": size === "md",
+                    "h-12": size === "lg",
+                },
+                {
+                    "px-2": size === "sm",
+                    "px-4": size === "md" || size === "lg",
+                }
+            )}
+            {...props}
+        />
+    );
 };
--- src/pages/email-verification/email-verification-page.tsx
+++ src/pages/email-verification/email-verification-page.tsx
@@ -1,12 +1,42 @@
 import { DerivLightIcEmailSentPasskeyIcon } from "@deriv/quill-icons/Illustration";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Input } from "../../components/base/input";
+import { useCountdown } from "usehooks-ts";
 
 export const EmailVerificationPage = () => {
+    const [email] = useState("placeholder@somedomain.com.my");
+    const [resend, setResend] = useState(false);
+    const [count, { startCountdown }] = useCountdown({
+        countStart: 60,
+    });
+
+    useEffect(() => {
+        if (resend) startCountdown();
+    }, [resend, startCountdown]);
+
     return (
         <React.Fragment>
-            <div className="min-h-[calc(100vh-52px)] flex flex-col items-center justify-center gap-y-4">
-                <DerivLightIcEmailSentPasskeyIcon height="120px" width="120px" />
-                <div className="">We've sent a verification code to </div>
+            <div className="h-[calc(100vh-84px)] flex flex-col justify-center gap-y-4">
+                <DerivLightIcEmailSentPasskeyIcon className="mx-auto" height="120px" width="120px" />
+                <div className="text-center font-bold leading-6 mx-auto">We've sent a verification code to {email}</div>
+                <Input size="lg" placeholder="Enter 6-digit code" block />
+                <div className="text-sm flex flex-col">
+                    <div className="justify-self-start mb-2">
+                        Didn't get a code?
+                        {resend ? (
+                            <span className="font-bold text-gray-400 ml-1">Resend code in {count} sec</span>
+                        ) : (
+                            <span className="ml-1 font-semibold underline" onClick={() => setResend(true)}>
+                                Resend code
+                            </span>
+                        )}
+                    </div>
+                    <ul className="list-disc text-sm leading-[18px] px-4">
+                        <li className="">Make sure you’ve entered your email correctly.</li>
+                        <li className="">Check your spam folder.</li>
+                        <li className="">Make sure the email isn’t blocked by firewalls or filters.</li>
+                    </ul>
+                </div>
             </div>
         </React.Fragment>
     );
